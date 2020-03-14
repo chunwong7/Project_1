2e5fae003dd7303b7633b9c5ee7205a004bb8b5b
unknown <ryanklueg@gmail.com>
2020-03-14 10:19:24 -0400

    cleaned up unnessesary files for this branch

--- sapie_data/data_conversion.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-years = range(3, 19)
-for year in years:
-    year = str(year)
-    if len(year) == 1:
-        year = f'0{year}'
-    read_file = pd.read_excel (f'est{year}all.xls')
-    read_file.to_csv (f'../sapie_data_csv/est{year}all.csv', index = None, header=True)
